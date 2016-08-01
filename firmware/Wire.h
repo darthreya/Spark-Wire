0b08e29fed26df1b4f4e01568c91a7958458c1bc
darthreya <athreya@umich.edu>
2016-08-01 18:02:37 -0400

    edited somestuff

--- firmware/Wire.h
+++ firmware/Wire.h
@@ -23,7 +23,18 @@
 #define TwoWire_h
 
 #include <inttypes.h>
-#include "Arduino.h"
+#if defined (SPARK)
+  #include "application.h"
+#else
+  #if defined(ARDUINO) && ARDUINO >= 100
+    #include "Arduino.h"
+  #else
+    #include "WProgram.h"
+  #endif
+  // here could follow some of the includes only needed on Arduino
+  // see bellow
+
+#endif
 
 #define BUFFER_LENGTH 32
 #define WIRE_HAS_END 1
